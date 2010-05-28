77ffd1cea623c5bf547887b4da63aa2f3a488f20
Kuleshov Alexander <kuleshovmail@gmail.com>
2010-05-28 19:22:13 +0600

    layout rename

--- src/layot.h
+++ src/layot.h
@@ -17,3 +17,12 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
+
+#ifndef LAYOUT_H
+#define LAYOUT_H
+
+#include <gtk/gtk.h>
+
+static GtkWidget* setup_layout (GtkUIManager *uimanager);
+
+#endif
